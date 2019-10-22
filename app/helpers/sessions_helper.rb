17f50c11572c96ecfb804e20efbaffce97318080
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-10-22 09:14:57 +0000

    finish login-function

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,26 @@
+module SessionsHelper
+  
+  # 引数に渡されたユーザーオブジェクトでログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue,そうでなければfalseを返す。
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  #セッションと@current_userwp破棄
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+end
