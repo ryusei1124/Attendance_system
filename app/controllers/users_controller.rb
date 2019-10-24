b8edb0b93c8b7d592dfcee8e18632e5587fa5904
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-10-24 13:46:31 +0000

    finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:destroy, :index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :admin_user, only: :destroy
+  
   def new
     @user = User.new
   end
   
   def show
-    @user = User.find(params[:id])
   end
   
   def create
@@ -18,9 +22,57 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # beforeフィルター
+    
+    # ログイン済みのユーザーか確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    # アクセスしたユーザーが現在ログインしているユーザー確認する
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # paramsハッシュからユーザーを取得する
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    # アクセスしたユーザーが現在ログインしているユーザー確認する
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -45,4 +45,20 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトする
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
